78cb2d77968997c01caa229755a9cb5df91ff5b3
Eugene Siow <kyo116@gmail.com>
2016-09-06 13:40:06 +0100

    Remove unnecessary imports

--- src/main/java/uk/ac/soton/ldanalytics/sparql2sql/util/FormatUtil.java
+++ src/main/java/uk/ac/soton/ldanalytics/sparql2sql/util/FormatUtil.java
@@ -1,8 +1,5 @@
 package uk.ac.soton.ldanalytics.sparql2sql.util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
